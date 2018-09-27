6be7eb0197e85088295a6111d25b40fd0f1ca1ff
tangcent <sweettom@foxmail.com>
2018-09-27 10:12:50 +0800

    ComponentEventBus

--- event-spring/event-spring-core/src/main/java/com/itangcent/event/spring/core/ComponentEventBus.java
+++ event-spring/event-spring-core/src/main/java/com/itangcent/event/spring/core/ComponentEventBus.java
@@ -1,4 +1,28 @@
 package com.itangcent.event.spring.core;
 
-public class ComponentEventBus {
+import com.itangcent.event.EventBus;
+
+import java.util.Arrays;
+
+public class ComponentEventBus implements EventBus {
+    private EventBus[] eventBuses;
+
+    public ComponentEventBus(EventBus[] eventBuses) {
+        this.eventBuses = eventBuses;
+    }
+
+    @Override
+    public String name() {
+        return "ComponentEventBus[" + Arrays.stream(eventBuses)
+                .map(EventBus::name)
+                .reduce((a, b) -> a + "," + b)
+                .orElse("") + "]";
+    }
+
+    @Override
+    public void post(Object event) {
+        for (EventBus eventBus : eventBuses) {
+            eventBus.post(event);
+        }
+    }
 }
